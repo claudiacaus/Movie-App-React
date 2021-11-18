521b6a66c8aa018ac255c6b6255fb4aa9605a251
Claudia <caucrah@gmail.com>
2021-11-19 00:53:00 +0100

    fixing the structure

---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect, useRef } from 'react';
+// API
+import API from '../API';
+
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+};
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(false);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState((prevState) => ({
+        ...movies,
+        results:
+          page > 1
+            ? [...prevState.results, ...movies.results]
+            : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // will run on the first render, and then every time the page is refreshed
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  return { state, loading, error };
+};
