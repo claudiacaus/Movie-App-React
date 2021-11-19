74ea3831842e132026e72aaed83afe2f7100fcf1
Claudia <caucrah@gmail.com>
2021-11-19 21:48:01 +0100

    Added the MovieInfo,BreadCrumb and Movie component

--- movie-app-react/src/components/Movie.js
+++ movie-app-react/src/components/Movie.js
@@ -5,6 +5,8 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
 import Grid from './Grid';
 import Spinner from './Spinner';
+import BreadCrumb from './BreadCrumb';
+import MovieInfo from './MovieInfo';
 
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
@@ -16,11 +18,13 @@ const Movie = () => {
   const { movieId } = useParams(); // useParams is a hook that returns an object with the params from the url
   const { state: movie, loading, error } = useMovieFetch(movieId); // movieId is passed from the parent component
 
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong ...</div>;
 
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
     </>
   );
 };
--- movie-app-react/src/helpers.js
+++ movie-app-react/src/helpers.js
@@ -1,11 +1,12 @@
 // Convert time to hours and minutes
-export const calcTime = time => {
+export const calcTime = (time) => {
   const hours = Math.floor(time / 60);
   const mins = time % 60;
   return `${hours}h ${mins}m`;
 };
+
 // Convert a number to money formatting
-export const convertMoney = money => {
+export const convertMoney = (money) => {
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
